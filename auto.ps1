b5450bc1b2d5922d8ebbb21e49bb875b8aa02cf7
slim147 <62090141+slim147@users.noreply.github.com>
2020-09-02 21:30:12 -0500

    Update auto.ps1
    html update

--- auto.ps1
+++ auto.ps1
@@ -1,2 +1,2 @@
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "This is   $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "<body style='bgcolor:yellow';text-align:center;color:red'>This is   $($env:computername) !"
